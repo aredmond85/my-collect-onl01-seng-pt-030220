3dd9663999888ede30b704b9926a1b0b3936869f
Austin Redmond <aredmond85@gmail.com>
2020-03-09 20:15:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+#my_collect should take 1 argument
+#create collection array
+#use while loop
+#iterate through array/collection array
+#Put code from the block into the collection array (use yield)
 
-
+def my_collect(array)
+  collection = []
+  i = 0
+  while i < array.length
+    collection <<< yield(array[i])
+    i += 1
+  end
+  collection
+end
